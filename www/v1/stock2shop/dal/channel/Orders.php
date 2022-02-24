811103823d6684783ed3a62a64d31f07f10f0f8d
Reinhardt Zündorf <reinhardt.zundorf@gmail.com>
2022-02-24 15:40:45 +0200

    Update orders interface in connector.

--- www/v1/stock2shop/dal/channel/Orders.php
+++ www/v1/stock2shop/dal/channel/Orders.php
@@ -5,7 +5,6 @@ use stock2shop\vo;
 
 /**
  * Interface Orders
- *
  * @package stock2shop\dal\channel
  */
 interface Orders {
@@ -13,15 +12,37 @@ interface Orders {
     /**
      * Get
      *
+     * This method must define the workflow for getting orders from a channel.
+     *
+     * The '$token' parameter will either be an empty string "" or it may contain
+     * a string value with one of the `vo\ChannelOrder` properties to search on.
+     * When '$token' is an empty string: all orders must be returned.
+     *
+     * The '$limit' parameter specifies the maximum number of order objects to return.
+     * Your implementation will need to include conditional logic for this which is
+     * compatible with your channel's API.
+     *
+     * You may supplement the workflow here with the '$channel' object's meta property
+     * items. (holds an array of `vo\Meta` objects).
+     *
      * @param string $token
      * @param int $limit
      * @param vo\Channel $channel
-     * @return array
+     * @return vo\ChannelOrder[]
      */
     public function get(string $token, int $limit, vo\Channel $channel): array;
 
     /**
      * Get By Code
+     *
+     * The purpose of this method is to return an array of orders which have
+     * been transformed from the format used by the channel into `vo\ChannelOrder`
+     * objects.
+     *
+     * Only orders which match the `channel_order_code` of each order in the '$orders'
+     * array parameter must be returned by this method. If any of the orders passed
+     * to this function do not have a `channel_order_code` set, then they are ignored
+     * and the function must continue.
      *
      * @param vo\ChannelOrder[] $orders
      * @param vo\Channel $channel
@@ -33,7 +54,7 @@ interface Orders {
      * Transform
      *
      * This method should define the workflow for converting a "webhook" order
-     * coming into Stock2Shop into a vo\ChannelOrder object.
+     * coming into Stock2Shop into a `vo\ChannelOrder` object.
      *
      * @param mixed $webhookOrder
      * @param vo\Channel $channel
@@ -42,3 +63,4 @@ interface Orders {
     public function transform($webhookOrder, vo\Channel $channel): vo\ChannelOrder;
 
 }
+
