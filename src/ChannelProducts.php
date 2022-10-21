1144282ae022a62688e552ce9749747c4d871bd1
Arata Yano <aratayano00@gmail.com>
2022-10-21 14:39:20 +0200

    ChannelProduct refactor

--- src/ChannelProducts.php
+++ src/ChannelProducts.php
@@ -4,17 +4,26 @@ declare(strict_types=1);
 
 namespace Stock2Shop\Connector;
 
+use GuzzleHttp\Exception\GuzzleException;
 use Stock2Shop\Share;
 
 class ChannelProducts implements Share\Channel\ChannelProductsInterface
 {
+    public const CHANNEL_META_URL_KEY = 'api_url';
+
     public function sync(
         Share\DTO\ChannelProducts $channelProducts,
         Share\DTO\Channel         $channel
     ): Share\DTO\ChannelProducts {
-        // keep track of which codes need to be deleted
-        $deleteCodes   = [];
-        $touchProducts = [];
+        $url = self::getServerUrlFromChannelMeta($channel->meta);
+        if ($url == "") {
+            return new Share\DTO\ChannelProducts([]);
+        }
+        $api = new DemoAPI\API($url);
+
+        // create payloads
+        $toDelete = [];
+        $toTouch  = [];
         foreach ($channelProducts->channel_products as $product) {
             if ($product->delete) {
                 // product is to be deleted from the channel
@@ -22,52 +31,32 @@ class ChannelProducts implements Share\Channel\ChannelProductsInterface
                     // we can only delete by channel_product_code
                     // if it is not set we assume that the product has
                     // not yet been synced
-                    $deleteCodes[] = $product->channel_product_code;
-                } else {
-                    // if no channel_product_code is set,
-                    // treat the product as if it has been
-                    // successfully deleted
-                    Helper::setDeleted($product);
+                    $toDelete[] = $product->channel_product_code;
                 }
             } else {
-                $touchProducts[] = Helper::createPayloadProduct($product);
+                $toTouch[] = Transform::DtoToDemoProduct($product);
             }
         }
 
         // create/update products
-        if (count($touchProducts) > 0) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'http://localhost:1234/products');
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($touchProducts));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $server_output = curl_exec($ch);
-            curl_close($ch);
-            $data = json_decode($server_output);
-            if ($server_output) {
-                Helper::setChannelProductFields($channelProducts, (array)$data);
+        if (count($toTouch) > 0) {
+            try {
+                $dps = $api->postProducts($toTouch);
+                Transform::SetChannelCodesFromDemoProducts($channelProducts, $dps);
+            } catch (GuzzleException $e) {
+                return new Share\DTO\ChannelProducts([]);
             }
         }
 
-
         // delete products
-        if (count($deleteCodes) > 0) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, 'http://localhost:1234/products');
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-            curl_setopt($ch, CURLOPT_HEADER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($deleteCodes));
-            curl_setopt($ch, CURLOPT_NOBODY, false);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $server_output = curl_exec($ch);
-            $code          = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            curl_close($ch);
-            if ((int)$code == 202) {
-                Helper::markAsDeleted($channelProducts, $deleteCodes);
-            }
-            $data = json_decode($server_output);
-            if ($server_output) {
-                Helper::setChannelProductFields($channelProducts, (array)$data);
+        if (count($toDelete) > 0) {
+            try {
+                $code = $api->deleteProducts($toDelete);
+                if ($code > 200 && $code < 300) {
+                    Transform::UnsetChannelProperties($channelProducts, $toDelete);
+                }
+            } catch (GuzzleException $e) {
+                return new Share\DTO\ChannelProducts([]);
             }
         }
 
@@ -79,52 +68,21 @@ class ChannelProducts implements Share\Channel\ChannelProductsInterface
         int               $limit,
         Share\DTO\Channel $channel
     ): Share\DTO\ChannelProducts {
-        // create url with params
-        $endpoint = 'http://localhost:1234/products/page?';
-        $params   = [
-            'channel_product_code' => $channel_product_code,
-            'limit'                => $limit,
-        ];
-        $url      = $endpoint . http_build_query($params);
-
-        // make get request to fetch product data from channel
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        $server_output = curl_exec($ch);
-        curl_close($ch);
-        $data = json_decode($server_output);
-
-        // create Share\DTO\ChannelProducts from response
-        $results = [
-            'channel_products' => []
-        ];
-        foreach ($data as $p) {
-            $cp                       = new Share\DTO\ChannelProduct([]);
-            $cp->success              = true;
-            $cp->channel_product_code = $p->id;
-            foreach ($p->variants as $variant) {
-                $cp->variants[] = new Share\DTO\ChannelVariant(
-                    [
-                        'sku'                  => $variant->sku,
-                        'success'              => true,
-                        'channel_variant_code' => $variant->id
-                    ]
-                );
-            }
-            foreach ($p->images as $image) {
-                $cp->images[] = new Share\DTO\ChannelImage(
-                    [
-                        'src'                => $image->source,
-                        'success'            => true,
-                        'channel_image_code' => $image->id
-                    ]
-                );
-            }
-            $results['channel_products'][] = $cp;
+        $url = self::getServerUrlFromChannelMeta($channel->meta);
+        if ($url == "") {
+            return new Share\DTO\ChannelProducts([]);
         }
-        return new Share\DTO\ChannelProducts($results);
+
+        $api = new DemoAPI\API($url);
+        try {
+            // get product data from the channel specified
+            $products = $api->getProducts($channel_product_code, $limit);
+
+            // transform DemoProduct data into ChannelProducts
+            return Transform::DemoProductToDto($products);
+        } catch (GuzzleException $e) {
+            return new Share\DTO\ChannelProducts([]);
+        }
     }
 
     public function getByCode(
@@ -140,42 +98,34 @@ class ChannelProducts implements Share\Channel\ChannelProductsInterface
             }
         }
 
-        // make get request to fetch product data from channel
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'http://localhost:1234/products');
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($codes));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $server_output = curl_exec($ch);
-        curl_close($ch);
-        $data = json_decode($server_output);
+        $url = self::getServerUrlFromChannelMeta($channel->meta);
+        if ($url == "") {
+            return new Share\DTO\ChannelProducts([]);
+        }
 
-        foreach ($channelProducts->channel_products as $value => $product) {
-            $product->success = false;
-            foreach ($data as $p) {
-                if ($product->channel_product_code == $p->id) {
-                    $product->success = true;
-                    foreach ($product->variants as $value => $variant) {
-                        $variant->success = false;
-                        foreach ($p->variants as $v) {
-                            if ($variant->channel_variant_code === $v->id) {
-                                $variant->success = true;
-                                break;
-                            }
-                        }
-                    }
-                    foreach ($product->images as $value => $image) {
-                        $image->success = false;
-                        foreach ($p->images as $i) {
-                            if ($image->channel_image_code === $i->id) {
-                                $image->success = true;
-                                break;
-                            }
-                        }
-                    }
-                }
+        $api = new DemoAPI\API($url);
+        try {
+            // get product data from the channel specified
+            $products = $api->getProductsByCodes($codes);
+
+            // transform DemoProduct data into ChannelProducts
+            return Transform::DemoProductToDto($products);
+        } catch (GuzzleException $e) {
+            return new Share\DTO\ChannelProducts([]);
+        }
+    }
+
+    /**
+     * @param Share\DTO\Meta[] $meta
+     */
+    private function getServerUrlFromChannelMeta(array $meta): string
+    {
+        foreach ($meta as $m) {
+            if ($m->key === self::CHANNEL_META_URL_KEY) {
+                return $m->value;
             }
         }
-        return $channelProducts;
+
+        return "";
     }
 }
