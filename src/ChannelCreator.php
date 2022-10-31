941b2fb371e6256526cbf7731f7b3a5cd144eb31
Arata Yano <aratayano00@gmail.com>
2022-10-31 10:16:17 +0200

    add abstract method

--- src/ChannelCreator.php
+++ src/ChannelCreator.php
@@ -7,6 +7,7 @@ namespace Stock2Shop\Connector;
 use Stock2Shop\Connector\Config\Environment;
 use Stock2Shop\Connector\Config\LoaderDotenv;
 use Stock2Shop\Share;
+use Stock2Shop\Share\Channel\ChannelOrdersInterface;
 
 class ChannelCreator extends Share\Channel\ChannelCreator
 {
@@ -19,5 +20,10 @@ class ChannelCreator extends Share\Channel\ChannelCreator
     public function createChannelProducts(): Share\Channel\ChannelProductsInterface
     {
         return new ChannelProducts();
+    }
+
+    public function createChannelOrders(): ChannelOrdersInterface
+    {
+        return new ChannelOrders();
     }
 }
